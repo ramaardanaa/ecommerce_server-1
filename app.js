e7a8c7828aa001027dca4033483dd246a1bdf5ac
ramaardanaa <ramaardanaa@gmail.com>
2020-11-14 18:10:33 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
 }
-
-
 const express = require('express')
+const app = express()
 const port = process.env.PORT || 3000
 const router = require("./routes/router")
 const cors = require("cors")
