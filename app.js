7ab337dc9683c8de0e2d35a282452f7850921a23
ramaardanaa <ramaardanaa@gmail.com>
2020-11-19 12:24:48 +0700

    deploy

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-// if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV === "development") {
   require("dotenv").config();
-// }
+}
 const express = require('express')
 const app = express()
-// const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000
 const router = require("./routes/router")
 const cors = require("cors")
 const errorHandler = require("./middlewares/errorHandler")
